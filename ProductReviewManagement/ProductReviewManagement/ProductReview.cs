1713004c996ddddbed535d9d165474b6e7bd177f
shaik ameer basha <shaikshabana758@gmail.com>
2022-03-02 13:06:49 +0530

    [shabana]AddingValues

---
+++ ProductReviewManagement/ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    internal class ProductReview
+    { 
+        public int productId { get; set; }
+        public int userId { get; set; }
+        public int rating { get; set; }
+        public string review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
+
---
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Product Review Management");
+            Console.WriteLine("Adding a Product Review In list");
+            Console.WriteLine("Enter Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Creating a list for Product Review
+            List<ProductReview> productReviews = new List<ProductReview>();
+            switch (option)
+            {
+                case 1:
+                    ProductReviewManager.AddingProductReview(productReviews);
+                    break;
+            }
+        }
+    }
+}
